823d6ee62aa21f2a0b1cc021136648c6d03b794f
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-05-01 13:58:30 -0300

    feat(permissions): add type infer to subjects and permissions

--- packages/auth/src/models/subjects/project.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const projectSubject = z.tuple([
-  z.union([z.literal('create'), z.literal('delete'), z.literal('manage')]),
-  z.literal('Project'),
-])
-
-export type ProjectSubject = z.infer<typeof projectSubject>
