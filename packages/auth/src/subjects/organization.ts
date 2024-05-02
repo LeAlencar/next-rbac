bf2d239e5dbd52db622bd3d8f75a766cea9cca3b
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-05-01 22:59:06 -0300

    feat(permissions): some changes

--- packages/auth/src/subjects/organization.ts
+++ packages/auth/src/subjects/organization.ts
@@ -5,8 +5,8 @@ import { organizationSchema } from '../models/organization'
 export const organizationSubject = z.tuple([
   z.union([
     z.literal('manage'),
-    z.literal('create'),
     z.literal('delete'),
+    z.literal('update'),
     z.literal('transfer_ownership'),
   ]),
   z.union([z.literal('Organization'), organizationSchema]),
