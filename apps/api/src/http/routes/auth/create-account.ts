be9d929221b9fe9532899b4d7292ebf6cebb9435
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-05-13 10:45:55 -0300

    feat(users): add create-account route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -10,12 +13,34 @@ export async function createAccount(app: FastifyInstance) {
         body: z.object({
           name: z.string().min(3).max(255),
           email: z.string().email(),
-          password: z.string().min(8).max(255),
+          password: z.string().min(6).max(255),
         }),
       },
     },
-    () => {
-      return 'User created!'
+    async (request, reply) => {
+      const { name, email, password } = request.body
+      const userExists = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userExists) {
+        return reply.status(400).send({
+          message: 'User already exists',
+        })
+      }
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
     },
   )
 }
