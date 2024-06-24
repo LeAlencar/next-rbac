59e98a81f94e2a12fa5195d4611617f05be739c2
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-06-24 20:39:52 -0300

    feat(invite): create invite

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -21,6 +21,7 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { createInvite } from './routes/invites/create-invite'
 import { getMembers } from './routes/members/get-members'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
@@ -91,6 +92,7 @@ app.register(updateProject)
 app.register(getMembers)
 app.register(updateMember)
 app.register(removeMember)
+app.register(createInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Server is running on port 3333')
